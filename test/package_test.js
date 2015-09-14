bd7c141a1bd2aac4cee47971fa3f5cec0a0440d8
Curtis Hatter <mitchell.hatter@gmail.com>
2015-09-14 11:41:06 -0400

    test that package exports strategy appropriately

---
+++ test/package_test.js
@@ -0,0 +1,12 @@
+var strategy = require('..');
+
+describe('passport-hmac', function() {
+  it('should export Strategy constructor directly from package', function() {
+    expect(strategy).to.be.a('function');
+    expect(strategy).to.equal(strategy.Strategy);
+  });
+
+  it('should export Strategy constructor', function() {
+    expect(strategy.Strategy).to.be.a('function');
+  });
+});
