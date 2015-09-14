006c316e3c26fdbd2e5766f94819a3369a2bc273
Curtis Hatter <mitchell.hatter@gmail.com>
2015-09-14 19:56:20 -0400

    test for exception during verification
    now at 100% code coverage

--- test/strategy_error_test.js
+++ test/strategy_error_test.js
@@ -3,29 +3,52 @@ var Strategy = require('../lib/strategy');
 
 describe('Strategy', function() {
   describe('that encounters an error during verification', function() {
-    var strategy = new Strategy(function(token, done) {
+    var strategy = new Strategy(function(publicKey, done) {
       return done(new Error('something went wrong'));
     });
 
-    describe('handling a request', function() {
-      var err;
+    var err;
 
-      before(function(done) {
-        chai.passport.use(strategy)
-          .error(function(e) {
-            err = e;
-            done();
-          })
-          .req(function(req) {
-            req.headers.authorization = 'Hmac bad_public_key:dGhpcyBpcyBhIHRlc3Q=';
-          })
-          .authenticate();
-      });
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.headers.authorization = 'Hmac bad_public_key:dGhpcyBpcyBhIHRlc3Q=';
+        })
+        .authenticate();
+    });
 
-      it('should error', function() {
-        expect(err).to.be.an.instanceOf(Error);
-        expect(err.message).to.equal('something went wrong');
-      });
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('something went wrong');
+    });
+  });
+
+  describe('that encounters an exception during verification', function() {
+    var strategy = new Strategy(function(publicKey, done) {
+      throw new Error('something went horribly wrong');
+    });
+
+    var err;
+
+    before(function(done) {
+      chai.passport.use(strategy)
+        .error(function(e) {
+          err = e;
+          done();
+        })
+        .req(function(req) {
+          req.headers.authorization = 'Hmac bad_public_key:dGhpcyBpcyBhIHRlc3Q=';
+        })
+        .authenticate();
+    });
+
+    it('should error', function() {
+      expect(err).to.be.an.instanceOf(Error);
+      expect(err.message).to.equal('something went horribly wrong');
     });
   });
 });
