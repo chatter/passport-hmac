625979308281ef7b1424285b274217814ae5e364
Curtis Hatter <mitchell.hatter@gmail.com>
2015-09-14 13:55:07 -0400

    update base strategy test

--- test/strategy_test.js
+++ test/strategy_test.js
@@ -2,9 +2,15 @@ var chai = require('chai');
 var HmacStrategy = require('../lib/strategy');
 
 describe('Strategy', function() {
-  var strategy = new HmacStrategy({}, function() {});
+  var strategy = new HmacStrategy(function() {});
 
   it('should be named hmac', function() {
     expect(strategy.name).to.equal('hmac');
+  });
+
+  it('should throw if constructed without a verify callback', function() {
+    expect(function() {
+      var s = new HmacStrategy();
+    }).to.throw(TypeError, 'HMAC Strategy requires a verify callback');
   });
 });
