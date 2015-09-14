8074dc00369ee69e18f944b5e33d46450f8da2a7
Curtis Hatter <mitchell.hatter@gmail.com>
2015-09-14 16:26:30 -0400

    updated strategy thanks to testing showing errors
    migrating away from crypto-js to nodes crypto package.
    
    added test for validating a request without content

--- test/strategy_test.js
+++ test/strategy_test.js
@@ -1,5 +1,6 @@
 var chai = require('chai');
 var HmacStrategy = require('../lib/strategy');
+var CryptoJS = require('crypto-js');
 
 describe('Strategy', function() {
   var strategy = new HmacStrategy(function() {});
@@ -12,5 +13,56 @@ describe('Strategy', function() {
     expect(function() {
       var s = new HmacStrategy();
     }).to.throw(TypeError, 'HMAC Strategy requires a verify callback');
+  });
+
+  describe('handling a request with valid credentials', function() {
+    var keys = { publicKey: 'public-key', privateKey: 'private-key' };
+
+    var strategy = new HmacStrategy(function(publicKey, done) {
+      if (publicKey === keys.publicKey) {
+        return done(null, {id: '1234', name: 'Test'}, keys.privateKey, { scope: 'read'});
+      }
+
+      return done(null, false);
+    });
+
+    var user;
+    var info;
+
+    before(function(done) {
+      chai.passport.use(strategy)
+        .success(function(u, i) {
+          user = u;
+          info = i;
+          done();
+        })
+        .req(function(req) {
+          req.headers.date = new Date().toUTCString();
+
+          var encString = CryptoJS.enc.Base64.stringify(
+            CryptoJS.HmacSHA1(
+              unescape(encodeURIComponent(
+                req.method + '\n' +
+                '\n' + // MD5 of req.body
+                '\n' + // Content-Type header
+                req.headers.date
+              )),
+              keys.privateKey
+          ));
+
+          req.headers.authorization = 'Hmac ' + keys.publicKey + ':' + encString;
+        })
+        .authenticate();
+    });
+
+    it('should supply user', function() {
+      expect(user).to.be.an.object;
+      expect(user.id).to.equal('1234');
+    });
+
+    it('should supply info', function() {
+      expect(info).to.be.an.object;
+      expect(info.scope).to.equal('read');
+    });
   });
 });
